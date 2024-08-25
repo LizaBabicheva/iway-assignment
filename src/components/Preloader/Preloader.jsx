2e08f257b4389f06eb2e87606868213930306e23
Liza Babicheva <elizavetababicheva@yandex.ru>
2024-08-25 22:28:55 +0300

    refactoring: ride info table layout improvement

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -2,11 +2,6 @@ import "./Preloader.scss";
 
 function Preloader() {
   return (
-    // <div className="preloader">
-    //   <div className="preloader-container">
-    //     <span className="preloader-round"></span>
-    //   </div>
-    // </div>
     <div className="loader-container">
       <div className="loader"></div>
     </div>
